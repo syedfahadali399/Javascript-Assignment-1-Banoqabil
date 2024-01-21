8dccc1578cceac4e5cd04aac9fb796753160b5c7
syed fahad ali <155745867+syedfahadali399@users.noreply.github.com>
2024-01-21 19:12:35 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+var Name = prompt('what is your name') ;
+var age = prompt('what is your age')
+
+console.log(Name);
+console.log(age);
+
+if(age >= 18){
+    console.log('You are a Student')
+} else{
+    console.log('You are not a Student')
+}
+
+alert('Welcome To My Website')
